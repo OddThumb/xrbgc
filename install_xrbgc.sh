4bfedf3d78522235fcc17c77f15ad7b58288c4e3
OddThumb <astrphscst@gmail.com>
2022-07-23 20:55:34 +0900

    Add messages

--- install_xrbgc.sh
+++ install_xrbgc.sh
@@ -4,8 +4,12 @@ echo "# XRBGC" >> ~/.bash_profile
 echo "export xrbgc=$(pwd)" >> ~/.bash_profile
 echo 'alias xginit="source $xrbgc/xrbgcinit.sh"' >> ~/.bash_profile
 
-echo "
+echo 'You need CIAO>=4.14 conda environment with name "ciao"
+
+Following commands are added in your ~/.bash_profile
+  export xrbgc=$(pwd)"
+  alias xginit="source $xrbgc/xrbgcinit.sh"
+
 To initialize, xrbgc,
   $ xginit
-You need CIAO-4.14 conda environment with name 'ciao'
-"
+'
